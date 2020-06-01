5eb1790c002b280881ee805c4de95bd99412cebe
Joanna Huang <joannathhuang@gmail.com>
2020-06-01 15:56:09 -0400

    [Feat]: Working on videos in list to be selected video

---
+++ src/components/Video-Card.js
@@ -0,0 +1,30 @@
+// import React from "react";
+
+// class VideoCard extends React.Component {
+//   constructor() {
+//     super();
+//     this.handleClick = this.handleClick.bind(this);
+//   }
+
+//   handleClick() {}
+
+//   render() {
+//     return (
+//       <div key={id} className="individual-videos">
+//         <img
+//           alt="pics"
+//           src={video.snippet.thumbnails.default.url}
+//           width="280"
+//           height="170"
+//         />
+//         <br />
+//         <div className="detail-container">
+//           <h4 className="video-title">{video.snippet.title}</h4>
+//           <p>{video.snippet.channelTitle}</p>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default VideoCard;
